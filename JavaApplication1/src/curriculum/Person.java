882c701602c0ffd49b0c5acf9c79e4221047529c
Cyb3rWolf945 <59710384+Cyb3rWolf945@users.noreply.github.com>
2024-10-01 12:23:05 +0100

    Update Person
    class person update.

--- JavaApplication1/src/curriculum/Person.java
+++ JavaApplication1/src/curriculum/Person.java
@@ -1,5 +1,15 @@
 package curriculum;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -14,19 +24,29 @@ public class Person {
     private int personId;
     private String nome;
     private int age;
-    private int curriculumId;
+    private String publicKey;
     
+    //public key
     
-    public Person(){
-        
-    }
-    
-    public Person(int personId, int age, int curriculumId, String nome){
+    /***
+     * Builds a person
+     * @param personId
+     * @param age
+     * @param nome 
+     */
+    public Person(int personId, int age, String nome){
         this.age = age;
         this.personId = personId;
-        this.curriculumId = curriculumId;
         this.nome = nome;
     }
+    
+    
+    /***
+    * Builds an empty person
+    */
+    public Person(){
+    }
+    
 
     /**
      * @return the personId
@@ -71,18 +91,93 @@ public class Person {
     }
 
     /**
-     * @return the curriculumId
+     * @return the publicKey
      */
-    public int getCurriculumId() {
-        return curriculumId;
+    public String getPublicKey() {
+        return publicKey;
     }
 
     /**
-     * @param curriculumId the curriculumId to set
+     * @param publicKey the publicKey to set
      */
-    public void setCurriculumId(int curriculumId) {
-        this.curriculumId = curriculumId;
+    public void setPublicKey(String publicKey) {
+        this.publicKey = publicKey;
     }
+    
+    /***
+     * Read from id and name from Users file, in the future will read the public key.
+     * @param path
+     * @return
+     * @throws FileNotFoundException
+     * @throws IOException 
+     */
+    public List<String> ReadUsers(String path) throws FileNotFoundException, IOException{
+        Event.fileExists f = new Event.fileExists();
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        ArrayList<String> ar1 = new ArrayList<>();
+
+        if (f.fileExists(path)){
+
+            try {
+                StringBuilder sb = new StringBuilder();
+                String line = br.readLine();
+                
+                while (line != null) {
+                    sb.append(line);
+                    sb.append(System.lineSeparator());
+                    ar1.add(sb.toString());
+                    line = br.readLine();
+                }
+                return ar1;
+                
+            } finally {
+                br.close();
+            }
+        }
+        return null;
+    }
+    
+    /***
+     * Checks if file exists
+     */
+    public static class fileExists{
+            public boolean fileExists(String path){
+                File f = new File(path);
+                return f.exists();
+        }
+    }
+    
+    /***
+     * Register user in file 
+     * @param path
+     * @param string
+     * @throws IOException 
+     */
+    public void SaveUser(String path, String string) throws IOException{
+        
+        ArrayList<String> teste = (ArrayList<String>) ReadUsers(path);
+        Event.fileExists f = new Event.fileExists();
+        BufferedWriter bw = new BufferedWriter(new FileWriter(path));
+        StringBuilder sb = new StringBuilder();   
+        if (f.fileExists(path)){
+            try{
+                for(String item: teste){
+                    sb.append(item);
+                }
+                
+                sb.append(string);
+                bw.write(sb.toString()+"\n");
+            }finally{
+                bw.close();
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return getPersonId() + " " + getNome();
+    }
+    
     
     
     
